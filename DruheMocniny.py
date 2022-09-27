594774217bb7bc93bf575ac04bf309fd50e94c5f
Jaroslav-Melichar <79139968+Jaroslav-Melichar@users.noreply.github.com>
2022-09-27 15:26:41 +0200

    2 ukoly

---
+++ DruheMocniny.py
@@ -0,0 +1,3 @@
+list=[1,2,3,4,5,6,7]
+mocnina = [num **2 for num in list]
+print("Mociny čísel od 1 do 7 jsou:",mocnina)
---
+++ ListCisel.py
@@ -0,0 +1,7 @@
+list=[1,2,3,4,5,6];
+print("Zadej číslo:");
+i=int(input());
+if i in list:
+    print("Číslo",i,"je v seznamu");
+else:
+    print("Číslo",i,"není v seznamu");
